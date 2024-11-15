1c0e18d3ab20a0ba346618b2bb402899c5b26baf
pulcinostorto009 <riccardo.mazzucchelli@iismontalegenova.it>
2024-11-15 12:32:22 +0100

    negro

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
-
-let contenitore = document.createElement('div');
-contenitore.style.borderStyle = "solid"
-contenitore.style.width = "70px"
-contenitore.style.height = "70px" 
-contenitore.style.position = "absolute"
-contenitore.style.top = "500px"
-contenitore.style.left = "500px"
+for (const elemento of elementi) {
+    let contenitore = document.createElement('div');
+    contenitore.style.borderStyle = "solid"
+    contenitore.style.width = "70px"
+    contenitore.style.height = "70px" 
+    contenitore.style.position = "absolute"
 
 
-let numeroAtomico= document.createElement('span');
-numeroAtomico.innerText = '1';
 
-let simbolo = document.createElement('a')
-simbolo.innerText = 'H'
-simbolo.style.fontSize = "40px"
-simbolo.href = 'https://it.wikipedia.org/wiki/Idrogeno'
-let nomeElemento = document.createElement('span');
-nomeElemento.innerText = 'idrogeno'
+    contenitore.style.top = 70 * elemento.ypos + "px"
+    contenitore.style.left = 70 * elemento.xpos + "px"
+    contenitore.style.boxSizing = "border-box"
+    contenitore.style.padding = "2px"
+contenitore.style.backgroundColor ="#" + elemento ["cpk-hex"]
+    let numeroAtomico= document.createElement('span');
+    numeroAtomico.innerText = elemento.number;
+    numeroAtomico.style.display = "block";
 
-contenitore.appendChild(numeroAtomico);
-contenitore.appendChild(simbolo);
-contenitore.appendChild(nomeElemento);
+    let simbolo = document.createElement('a')
+    simbolo.innerText = elemento.symbol;
+    simbolo.style.fontSize = "25px"
+    simbolo.href = elemento.source;
+    simbolo.style.display = "block";
+    simbolo.style.textAlign = "center"
 
-let contenutoPrincipale = document.getElementById('contenuto-principale');
 
-contenutoPrincipale.appendChild(contenitore);
+    let nomeElemento = document.createElement('span');
+    nomeElemento.innerText = elemento.name;
+    nomeElemento.style.fontSize = "11px"
+    nomeElemento.style.display = "block";
+    nomeElemento.style.textAlign = "center"
+   
+    contenitore.appendChild(numeroAtomico);
+    contenitore.appendChild(simbolo);
+    contenitore.appendChild(nomeElemento);
+    
+    let contenutoPrincipale = document.getElementById('contenuto-principale');
+    contenutoPrincipale.style.position = "relative"
+    contenutoPrincipale.appendChild(contenitore); 
+}
+
+
+
+
+
 
 
 
